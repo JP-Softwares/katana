0939d20ae0307850a1b481f8235d243c40489814
Andre-Luiz01 <andreff.net@gmail.com>
2025-06-17 08:32:26 -0300

    minor adjustment

--- katana/src/main/java/com/jpsoftwares/katana/DTO/ProfissionalDTO/ReturnProfissionalDTO.java
+++ katana/src/main/java/com/jpsoftwares/katana/DTO/ProfissionalDTO/ReturnProfissionalDTO.java
@@ -5,5 +5,5 @@ import com.jpsoftwares.katana.model.Roles;
 
 
 
-public record ReturnProfissionalDTO(Long id, String nome, String email, Roles role, String tipo, BasicEmpresaDTO basicEmpresaDTO) {
+public record ReturnProfissionalDTO(Long id, String nome, String email, Roles role, String tipo, BasicEmpresaDTO empresa) {
 }
